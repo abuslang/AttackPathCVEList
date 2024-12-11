1f3b4f31c56dce4b6aae79d0d691ae342f74da09
Abdus Quadri <asquadri1@gmail.com>
2024-12-11 17:07:52 -0500

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ api_key = ""                    # your public key
 api_secret = ""                 # your private key
 
 url = "api3.prismacloud.io"     # your url ex. "api2.prismacloud.io". DONT add https:// or a / at the end  
-                                # I think M&T would be api3
+                                # I think you would be api3
